3b320c19847e01bc7eeb9761b73f5ee01a271507
lloydms <lloydms83@gmail.com>
2024-02-29 12:41:32 -0500

    added targets to target group

--- main.tf
+++ main.tf
@@ -67,9 +67,15 @@ module "alb" {
   target_groups = {
     ex-instance = {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   }
 
