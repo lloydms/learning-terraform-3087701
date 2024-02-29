0b8d548a58f924c10823e3b60c8b62223f7884fc
lloydms <lloydms83@gmail.com>
2024-02-29 15:23:50 -0500

    made target group arns a list

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.4.0"
 
@@ -40,7 +40,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = [module.blog_alb.target_group_arns]
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
