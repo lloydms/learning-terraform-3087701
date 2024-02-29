ecf695f413c6384f3b1bc946dcd649af20e6dd24
lloydms <lloydms83@gmail.com>
2024-02-29 13:33:34 -0500

    re-entering target groups input

--- main.tf
+++ main.tf
@@ -64,6 +64,15 @@ module "alb" {
     }
   }
 
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
   tags = {
     Environment = "dev"
   }
